600d9df22ae1e1885997bf8737456b55d50988b2
herpie <herpritsingh12@gmail.com>
2021-12-04 17:24:49 +0100

    added guid to the event

--- ProductMS.Application/Services/EventBus/Event.cs
+++ ProductMS.Application/Services/EventBus/Event.cs
@@ -3,9 +3,12 @@
     public abstract class Event
     {
         public DateTime TimeStamp { get; protected set; }
+
+        public Guid Guid { get; protected set; }
         protected Event()
         {
             TimeStamp = DateTime.Now;
+            Guid = Guid.NewGuid();  
         }
     }
 }
